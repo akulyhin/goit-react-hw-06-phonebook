ce9fda244c8af3a112a1ad9f1aa65ba72779ef74
Artem Kulyhin <akulyhin@gmail.com>
2021-02-10 18:03:53 +0200

    Add redux-toolkit

--- src/redux/reducers/contactsReducer.js
+++ src/redux/reducers/contactsReducer.js
@@ -1,40 +1,28 @@
-import {combineReducers} from 'redux';
-import { ADDTASK, REMOVETASKS, SETFILTER } from '../constants/todoConstants';
-
+import { createReducer } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { ADDTASK, REMOVETASKS, SETFILTER } from "../constants/todoConstants";
 
 const initialState = [
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
 
-const contactsReducer = (state=[...initialState], action) => {
-    switch (action.type) {
-        case ADDTASK:
-            return [...state, action.payload];
+const contactsReducer = createReducer([...initialState], {
+  [ADDTASK]: (state, action) => [...state, action.payload],
+  [REMOVETASKS]: (state, action) => [
+    ...state.filter((item) => item.id !== action.payload),
+  ],
+});
 
-            case REMOVETASKS:
-                return [...state.filter(item => item.id !== action.payload)];
-    
-        default:
-           return state;
-    }
-}
-
-const filterReducer = (state="", action) => {
-    switch (action.type) {
-        case SETFILTER:
-            return action.payload;
-    
-        default:
-           return state;
-    }
-}
+const filterReducer = createReducer("", {
+  [SETFILTER]: (state, action) => action.payload,
+});
 
 const reducer = combineReducers({
-   items: contactsReducer,
-   filter: filterReducer
-})
+  items: contactsReducer,
+  filter: filterReducer,
+});
 
 export default reducer;
